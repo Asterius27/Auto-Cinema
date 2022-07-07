53d12dc1f451c4b16fde21b0005a944e0d32097c
Asterius27 <878352@stud.unive.it>
2022-07-07 15:40:32 +0200

    first commit

---
+++ app/src/main/java/com/example/autocinema/CarService.java
@@ -0,0 +1,11 @@
+package com.example.autocinema;
+
+import com.google.android.apps.auto.sdk.CarActivity;
+import com.google.android.apps.auto.sdk.CarActivityService;
+
+public class CarService extends CarActivityService {
+    @Override
+    public Class<? extends CarActivity> getCarActivity() {
+        return HelloWorld.class;
+    }
+}
---
+++ app/src/main/java/com/example/autocinema/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example.autocinema;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.google.android.apps.auto.sdk.CarActivity;
+
+public class HelloWorld extends CarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+        getCarUiController().getStatusBarController().setTitle("Hello AA");
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.youtube.com/");
+    }
+}
